c4594becb8ccaa36dab2c898100bc3ce872a3da9
Estu <weramadian@gmail.com>
2024-03-15 01:50:48 +0700

    feat : add movies and series page

---
+++ src/app/movies/page.js
@@ -0,0 +1,21 @@
+"use client"
+import { FilmCard } from "@/components";
+import useAlertStore from '@/states/alertState';
+import { Stack, Typography } from "@mui/material";
+
+const page = () => {
+  const { handleOpenAlert } = useAlertStore();
+
+  const handleClick = () => {
+    handleOpenAlert();
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Typography variant="h5">Movies</Typography>
+      <FilmCard onClick={handleClick} />
+    </Stack>
+  );
+};
+
+export default page;
